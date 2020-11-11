92da35f7aa6e152115b8843726f0dc3f815c330c
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-11-10 22:29:20 -0500

    update cors

--- src/server.js
+++ src/server.js
@@ -16,6 +16,13 @@ io.on("connection", socket => {
 
 });
 app.io = io;
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    next();
+});
 app.use(routes);
 app.use(function (req, res, next) {
     req.io = io;
