180ddbd9d8cf0f677b6151ae6c594a1a3c84aa23
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-11-13 01:46:45 -0500

    update user table

--- src/database/seeds/001_users.js
+++ src/database/seeds/001_users.js
@@ -5,7 +5,8 @@ exports.seed = function (knex) {
             email: "dasilvasantosluisfelipe@gmail.com",
             password: "$2a$08$otr68DjxydTQKIpbnduuz.V.9Afp/C2nB3VPHyenOtc/fIFLlO/Iy",
             name:'Luis Santos',
-            profile:'manager'
+            profile:'manager',
+            type:'empresa'
         }
     ])
 }
